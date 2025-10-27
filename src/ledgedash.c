e273776a056a3c6f580b42c8c1358f15f41a16f0
Ben Haines <bhainesva@gmail.com>
2025-10-26 20:35:00 -0400

    Add random start position option to ledgedash event

--- src/ledgedash.c
+++ src/ledgedash.c
@@ -49,11 +49,12 @@ enum reset_pos
     OPTPOS_FALLING,
     OPTPOS_STAGE,
     OPTPOS_RESPAWNPLAT,
+    OPTPOS_RANDOM,  // Must be last
 };
 
 // Main Menu
 static const char *LdshOptions_CamMode[] = {"Normal", "Zoom", "Fixed", "Advanced"};
-static const char *LdshOptions_Start[] = {"Ledge", "Falling", "Stage", "Respawn Platform"};
+static const char *LdshOptions_Start[] = {"Ledge", "Falling", "Stage", "Respawn Platform", "Random"};
 static float LdshOptions_GameSpeeds[] = {1.f, 5.f/6.f, 2.f/3.f, 1.f/2.f, 1.f/4.f};
 static const char *LdshOptions_GameSpeedText[] = {"1", "5/6", "2/3", "1/2", "1/4"};
 static const char *LdshOptions_Reset[] = {"None", "Same Side", "Swap", "Swap on Success", "Random"};
@@ -956,7 +957,11 @@ void Fighter_PlaceOnLedge(void)
     hmn_data->facing_direction = ledge_dir;
 
     // check starting position
-    switch (LdshOptions_Main[OPT_POS].val)
+    enum menu_options start_pos = LdshOptions_Main[OPT_POS].val;
+    if (start_pos == OPTPOS_RANDOM) {
+        start_pos = (enum menu_options)(HSD_Randi(OPTPOS_RANDOM));
+    } 
+    switch (start_pos)
     {
     case OPTPOS_LEDGE:
     {
