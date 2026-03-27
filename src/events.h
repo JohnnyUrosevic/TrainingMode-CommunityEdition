f1174d9b3cc2f437591601677810a064cbf8910e
Alex Harrison <alexander.harrison.r@gmail.com>
2026-03-26 20:37:34 -0600

    bump version v1.3 d7

--- src/events.h
+++ src/events.h
@@ -6,8 +6,8 @@
 #include "savestate.h"
 #include <stdint.h>
 
-#define TM_VERSSHORT "TM-CE v1.3 d6"
-#define TM_VERSLONG "TM Community Edition v1.3 d6"
+#define TM_VERSSHORT "TM-CE v1.3 d7"
+#define TM_VERSLONG "TM Community Edition v1.3 d7"
 #define EVENT_DATASIZE 512
 #define TM_FUNC -(50 * 4)
 
